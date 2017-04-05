565c596f6bb83de7b60573bd5a705d4f6a1124fa
Sharang Wakankar <sharang.wakankar@hotmail.com>
2017-04-04 23:48:01 -0700

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -49,14 +49,18 @@ angular.module("PokeModule").controller("Data1Controller", function(MyData1Servi
 			
 			var halfHeight = response.data.height/20;
 			
-			var totalStats = 0;
+			var avgExp = response.data.base_experience/2;
+			
+			var avgTotalStats = 0;
 			
 			for (i = 0; i < 6; i++) {
 				
-				totalStats += response.data.stats[i].base_stat;
+				avgTotalStats += response.data.stats[i].base_stat;
 			}
 			
-			MyData1Service.setData(response.data.id, response.data.base_experience, totalStats, halfWeight, halfHeight);	
+			avgTotalStats /= 2;
+			
+			MyData1Service.setData(response.data.id, avgExp, avgTotalStats, halfWeight, halfHeight);	
 		});	
 		
 		
@@ -82,20 +86,41 @@ angular.module("PokeModule").controller("Data2Controller", function(MyData2Servi
 			
 			var halfHeight = response.data.height/20;
 			
-			var totalStats = 0;
+			var avgExp = response.data.base_experience/2;
+			
+			var avgTotalStats = 0;
 			
 			for (i = 0; i < 6; i++) {
 				
-				totalStats += response.data.stats[i].base_stat;
+				avgTotalStats += response.data.stats[i].base_stat;
 			}
 			
-			MyData2Service.setData(response.data.id, response.data.base_experience, totalStats, halfWeight, halfHeight);	
+			avgTotalStats /= 2;
+			
+			MyData2Service.setData(response.data.id, avgExp, avgTotalStats, halfWeight, halfHeight);	
 		});	
 	}
 	
 	this.getPoke = function() {
 		
 		return MyData2Service.getData();
+	}
+	
+});
+
+angular.module("PokeModule").controller("NameController", function(MyNameService) {
+
+	var self = this;
+	
+	self.setName = function(name) {
+		
+		MyNameService.setData(name);
+	}
+	
+	self.getName = function() {		
+
+		console.log(MyNameService.getData());
+		return MyNameService.getData();
 	}
 	
 });
@@ -222,7 +247,7 @@ angular.module("PokeModule").controller("GetChoiceController", function(MyChoice
 		
 		if (MyChoice3Service.getPick3() === 1) {
 			console.log("woo 3");			
-			this.image3 = 'https://vignette2.wikia.nocookie.net/xenonauts/images/4/4c/Machinegun.png/revision/latest?cb=20121206094123';
+			this.image3 = 'http://vignette4.wikia.nocookie.net/bioshock/images/e/e0/Machine_Gun.png/revision/latest?cb=20091201194803';
 		} 
 		
 		if (MyChoice4Service.getPick4() === 1) {
@@ -241,40 +266,235 @@ angular.module("PokeModule").controller("GetChoiceController", function(MyChoice
 	
 });
 
-angular.module("PokeModule").controller("OpponentController", function() {
+angular.module("PokeModule").controller("OpponentController", function(MyPokeService) {
 	
-	this.oppImage = "";
+	var oppData = this;
 	
-	this.showTheOpponent = function() {		
+	oppData.oppImage = "";
+	oppData.numRand = 0;
 	
-		this.oppImage = 'pics/'+randomNum()+'.png';
- 		
+	oppData.showOpponent = function() {
+		
+		oppData.numRand =  Math.floor(Math.random() * 720);
+		console.log(oppData.numRand);
+		oppData.oppImage = 'pics/'+oppData.numRand+'.png';
+		
 	}
 	
-	function randomNum() {
+	oppData.prepareForBattle = function(pokExp) {	
 		
-		return Math.floor(Math.random() * 720);
+		console.log("pokExp " + pokExp + " opp " + oppData.numRand);
+			
+		var promise = MyPokeService.sendRequestForPokemon(oppData.numRand);
+			
+		promise.then(function(response) {
+				
+			console.log(response.data);
+			oppData.oppExp = response.data.base_experience;
+			console.log(oppData.oppExp);
+			
+			if (oppData.oppExp > pokExp) {
+				
+				oppData.battle(-1);
+				
+			} else if (oppData.oppExp < pokExp) {
+				
+				oppData.battle(1);
+				
+			} else if (oppData.oppExp == pokExp) {
+				
+				oppData.battle(0);
+				
+			}
+				
+		}, function(response) {
+				
+		});
+			
+	}	
+		
+	oppData.battle = function(exp) {
+	
+		console.log(exp);
+		
+		var elemOpp = document.getElementById("aniOpp");
+		var posOpp = 670;
+		var posOppTop = 120;
+		
+		var elemHead = document.getElementById("aniNewHead");
+		var posH = 150;	
+		var posHTop = 200; 
+		
+		var elemBottom = document.getElementById("aniNewBot");
+		var posB = 150;
+		var posBTop = 100;
+		
+		var idOpp = setInterval(moveOpp, 10);
+		var idH = setInterval(moveHead, 10);
+		var idB = setInterval(moveBottom, 10);		
+		
+		function moveOpp() {
+			if (posOpp == 400 || posOppTop == 250) {
+				clearInterval(idOpp);
+			} else {
+				posOpp--; 				
+				
+				if (exp == 1) {
+					
+					posOppTop++;					
+					elemOpp.style.zIndex = "-2";
+				    elemOpp.style.top = posOppTop + 'px';
+				    
+				} else if (exp == 0) {
+					console.log("TIE!");
+				}
+				
+			    elemOpp.style.left = posOpp + 'px';
+			}
+		}					
+		
+		function moveHead() {
+			if (posH == 450 || posHTop == 450) {
+				clearInterval(idH);
+			} else {
+				posH++; 
+				
+				if (exp == -1) {
+					
+					posHTop++;
+					elemHead.style.zIndex = "-2";
+					elemHead.style.top = posHTop + 'px';
+					
+				}
+				
+			    elemHead.style.left = posH + 'px'; 
+			}
+		}		
+		
+		function moveBottom() {
+			if (posB == 450 || posBTop == 350) {
+				clearInterval(idB);
+			} else {
+				posB++; 
+				
+				if (exp == -1) {
+					
+					posBTop++;
+					elemBottom.style.zIndex = "-2";
+					elemBottom.style.top = posBTop + 'px';
+					
+				}
+
+			    elemBottom.style.left = posB + 'px'; 
+			}
+		}			
 	}
 	
-	this.battle = function() {
+	oppData.showSaber = function() {
 		
-		if (randomNum() %2 === 0) {
-			
-			this.winner = "You Win!";
-			
-		} else {
-			
-			this.winner = "You Lose!";
+		var elemSaber = document.getElementById("aniNewSaber");
+		var posS = 260;
+		var posSmore = 470;
+		var idS = setInterval(moveSaber, 50);	
+		var idSmore;
+		
+		function moveSaber() {
+			if (posS == 160) {				
+				clearInterval(idS);
+				idSmore = setInterval(moveSaberMore, 50);
+			} else {
+				posS--;  
+			    elemSaber.style.top = posS + 'px'; 
+			}
+		}
+		
+		function moveSaberMore() {
+			if (posSmore == 670) {				
+				clearInterval(idSmore);
+			} else {
+				posSmore++;  
+			    elemSaber.style.left = posSmore + 'px'; 
+			}
+		}
+	}
+	
+	oppData.showBat = function() {
+		
+		var elemBat = document.getElementById("aniNewBat");
+		var posBa = 200;
+		var idBa = setInterval(moveBat, 10);	
+		
+		function moveBat() {
+			if (posBa == 600) {
+				clearInterval(idBa);
+			} else {
+				posBa++;  
+			    elemBat.style.left = posBa + 'px'; 
+			}
+		}
+		
+	}
+	
+	oppData.showGun = function() {
+		
+		var elemGun = document.getElementById("aniNewGun");
+		var posG = 360;
+		var posGdown = 330;
+		var idG = setInterval(moveGun, 10);	
+		var idGdown;
+		
+		function moveGun() {
+			if (posG == 330) {
+				clearInterval(idG);
+				idGdown = setInterval(moveGunDown, 10);
+			} else {
+				posG--;  
+			    elemGun.style.top = posG + 'px'; 
+			}
+		}
+		
+		function moveGunDown() {
+			if (posGdown == 360) {
+				clearInterval(idG);
+				idG = setInterval(moveGun, 10);
+			} else {
+				posGdown++;  
+			    elemGun.style.top = posGdown + 'px'; 
+			}
+		}
+	}
+	
+	oppData.showWild = function() {
+		
+		var elemWild = document.getElementById("aniNewWild");
+		var posW = 200;
+		var idW = setInterval(moveWild, 10);	
+		
+		function moveWild() {
+			if (posW == 400) {
+				clearInterval(idW);
+			} else {
+				posW++;  
+			    elemWild.style.left = posW + 'px'; 
+			}
 		}
 		
 	}
 	
 });
+	//oppData.winner = "LOLOL";
+
+angular.module("PokeModule").controller("ShowOpponentController", function($scope) {
 	
+	$scope.unhideOpp = false;
+	
+});
+
 angular.module("PokeModule").controller("ShowStatsController", function($scope) {
 	
 	$scope.unhideStats = false;
 	
+	/* Useless: */
 	$scope.printStats = function(divName) {
 		
 		 var printContents = document.getElementById(divName).innerHTML;
@@ -292,12 +512,3 @@ angular.module("PokeModule").controller("TestController", function($scope, MySim
 	$scope.name = MySimpleService.name;	
 	
 });
-
-angular.module("PokeModule").controller("ResultCtrl", function() {
-	
-});
-
-
-
-
-
